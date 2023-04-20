43a0b7880b12b0fe28e043bc3ce8a8aac2287221
Davi SAS <ivad8080@gmail.com>
2023-04-20 20:43:53 -0300

    update

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ const h1 = document.createElement('h1')
 h1.textContent = 'Users'
 root.appendChild(h1)
 
+// for ( const person of people ) {
+//   const p = document.createElement('p')
+//   p.textContent = person.name
+//   root.appendChild(p)
+// }
+
+for ( let i = 0; i < people.length; i++ ) {
+  const p = document.createElement('p')
+  p.textContent = people[i].name
+  root.appendChild(p)
+}
